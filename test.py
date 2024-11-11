38310d6fa34dc13d66297bb5a007434bb4b01bd7
giovannicerlisio <giovannicerlisio31@gmail.com>
2024-11-11 22:48:47 +0100

    jenkins file

--- test.py
+++ test.py
@@ -35,3 +35,13 @@ class TestWebsite:
     self.browser.close()
     self.browser.quit()
 
+  def test_tools_menu(self):
+    """this test checks presence of Developer Tools menu item"""
+    tools_menu = self.browser.find_element(By.XPATH,
+                                           "//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']")
+
+    tools_menu.click()
+
+    menu_popup = self.browser.find_element(By.CSS_SELECTOR, "div[data-test='main-submenu']")
+    assert menu_popup is not None
+
